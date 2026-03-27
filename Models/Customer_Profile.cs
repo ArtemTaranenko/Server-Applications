16e8f6c9c3038ed1f3b84de02693c07767dda067
Admin <artemtaranenko2807@gmail.com>
2026-03-27 21:55:33 +0100

    Lab4-Zad1

---
+++ Models/Customer_Profile.cs
@@ -0,0 +1,12 @@
+﻿namespace AS_Taranenko_lab1_gr1.Models
+{
+    public class Customer_Profile
+    {
+
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Phone {  get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
